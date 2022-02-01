8f2cf3c1ebccf0e5b1d09e5f36de93f2b0cc36a3
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2022-02-01 05:27:42 +0300

    was solved

---
+++ Min-Max_Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Min_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "1 3 5 7 9";
+            var str2 = "256741038 623958417 467905213 714532089 938071625";
+
+            var str3 = "1 2 3 4 5";
+
+
+            List<int> arr = str3.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            miniMaxSum(arr);
+        }
+
+        private static void miniMaxSum(List<int> arr)
+        {
+            if (arr.Count > 0)
+            {
+                long minRes = 0;
+                long maxRes = 0;
+
+                arr.Sort();
+                for(int i=0;i<arr.Count;i++)
+                {
+                    if (i != 0)
+                        maxRes += arr[i];
+                    if (i != arr.Count)
+                        minRes += arr[i];
+
+                }
+
+
+                Console.WriteLine($"{minRes} {maxRes}");
+            }
+        }
+    }
+}
